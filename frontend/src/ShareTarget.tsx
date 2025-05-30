cbfc6c5371a6150d88161b1be01b33d7193b37de
Richard Bilson <rcbilson@gmail.com>
2025-05-30 09:33:47 -0400

    typescript

--- frontend/src/ShareTarget.tsx
+++ frontend/src/ShareTarget.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // This component acts as a PWA share target. It reads the shared URL from the POSTed form data
